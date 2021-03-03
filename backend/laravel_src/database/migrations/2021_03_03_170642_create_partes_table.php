b6d07503992d8f073a40347bee4f6e8211375611
despinozap <despinozap.dev@gmail.com>
2021-03-03 15:27:48 -0300

    UPDATE: Backend - Clientes, Estadosolicitudes, Marcas and Partes created (migrations and models)

---
+++ backend/laravel_src/database/migrations/2021_03_03_170642_create_partes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('marca_id')->unsigned();
+            $table->string('nparte');
+            $table->timestamps();
+
+            $table->foreign('marca_id')->references('id')->on('marcas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partes');
+    }
+}
